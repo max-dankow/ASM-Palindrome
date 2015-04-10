e550afdddef049b153ec8a5fd40c6fda5679e72c
Maxim Dankovtsev <max.dankow@gmail.com>
2015-04-11 01:52:41 +0300

    Work with SAMS, reading string, is_palindrome done

--- main.s
+++ main.s
@@ -1,50 +1,184 @@
 .data
-
-    #строка для тестирования распознавания палиндромов
-    test_str:
-        .string "abacaba\n"
-
+test_str:
+    .string "aba!aba"
+test_len:
+    .long . - test_str - 2
+msg:
+    .asciz "Hello, world!\n"
+    
+format:
+    .string "Num = %ld\n"
+    
+number:
+    .long 10
+    
+left:
+    .long 0
+    
+right:
+    .long 0
+    
 .text
-
+    .global main # entry point
+    
 /******************************************************************
-    Выводит строку, находящуюся в %eax, в stdout
+    Выводит строку %eax в stdout
 ******************************************************************/
-.type print_str, @function
-
+format_str:
+    .string "%s\n"
 print_str:
     pushl %eax
+    pushl $format_str
     call printf
+
+    popl %eax
     popl %eax
     ret
-#END OF print_str
+#END OF print_num 
+
+
+
+/******************************************************************
+    Выводит число %eax в stdout
+******************************************************************/
+format_str_num:
+    .string "%d\n"
+print_num:
+    pushl %eax
+    pushl $format_str_num
+    call printf
+
+    popl %eax
+    popl %eax
+    ret
+#END OF print_num   
+
+
+/******************************************************************
+    Выводит символ %al в stdout
+******************************************************************/
+format_str_char:
+    .string "%c\n"
+print_char:
+    pushw %ax
+    pushl $format_str_char
+    call printf
+
+    popl %eax
+    popw %ax
+    ret
+#END OF print_num  
 
 
 /******************************************************************
     Проверяет строку, адрес которой записан в %eax, на палиндром.
 ******************************************************************/
-.type is_palindrome, @function
-
 is_palindrome:
-  #напечатаем обрабатываемую строку
+    pushl %ecx
+    pushl %ebx
+    
     call print_str
+    movl test_len, %ebx
+    movl %ebx, right
+    
+    movl $50,  %ecx
+    loop_begin:
+        push %ecx
+      #выведем счетчики left и right  
+        push %eax
+        
+        movl left, %eax
+        call print_num
+        movl right, %eax
+        call print_num
+
+        pop %eax
+        
+        call print_str
+        xorl %edx, %edx
+        
+        movl %eax, %ebx
+        
+        xorl %eax, %eax
+        movl left, %ecx
+        movb (%ebx, %ecx), %dl
+        movb %dl, %al
+    
+        #call print_char
+        
+        xorl %eax, %eax
+        movl right, %ecx
+        movb (%ebx, %ecx), %dh
+        movb %dh, %al
+        #call print_char
+        
+        cmpb %dl, %dh
+        jne _fail_palindrome
+        
+        mov %ebx, %eax
+
+        incl left
+        decl right
+        
+        popl %ecx
+        movl left, %ebx 
+        cmp right, %ebx
+        #loop loop_begin 
+        jbe loop_begin
+    
+    mov $1, %eax
+    jmp _exit_palindrome
+_fail_palindrome:
+    mov $0, %eax
+    popl %ecx
+_exit_palindrome:
+    popl %ebx
+    popl %ecx
+    
     ret
 #END OF is_palindrome
 
 
-/******************************************************************
-   Основная программа
-******************************************************************/
-.globl _start
-.type _start, @function
+/*******************************************************************
+    Считать строку по адресу (%eax) из stdin
+*******************************************************************/
+format_str_in:
+    .string "%s"
+read_str:
+    pushl %eax
+    pushl $format_str_in
+    call scanf
 
-_start:
+    popl %eax
+    popl %eax
+    ret
+#END OF read_str
 
-  #проверка строки test_str на палиндром
-    mov $test_str, %eax
-    call is_palindrome
+main:
+    movl %esp, %ebp #for correct debugging
+  #печать строки "Hello World!"
+    movl $msg, %eax
+    call print_str
+    
+  #считать строку из stdin
+    movl $str1, %eax
+    call read_str
+    
+  #проверка на палиндром
+    mov $str1, %eax
+    call is_palindrome  
+    call print_num
+    
+  #выход из программы
+    addl $4, %esp
+    xorl %eax, %eax
+    call exit
+    
+.bss
 
-  #завершение программы
-    mov $1, %eax
-    mov $0, %ebx
-    int $0x80
-#END OF _start
+str1:
+    .space 256
+    
+str2:
+    .space 256
+
--- makefile
+++ makefile
@@ -1,9 +1,6 @@
 all:prog
 
-prog:main.o
-	ld -dynamic-linker /lib/ld-linux.so.2 -o prog main.o -lc
-
-main.o:main.s
-	as -g main.s -o main.o
+prog:main.s
+	gcc main.s -o prog
 
 main.s:
